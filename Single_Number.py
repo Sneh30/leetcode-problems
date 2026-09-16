a7f296c8aebb28e908fa78a60316f5e68374dcf5
Sneh30 <snehsinha30@gmail.com>
2026-09-16 22:49:46 +0530

    Add Single Number solution

---
+++ Single_Number.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/single-number/
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        ans=0
+        for num in nums:
+            ans=ans^num
+        return ans
