fa61d80020b183ca1f668c0b3da597a4bbbc4236
Sneh30 <snehsinha30@gmail.com>
2026-09-20 19:17:02 +0530

    Add Reverse Linked List solution (problem 206)

---
+++ Reverse_Linked_List.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/reverse-linked-list/
+class Solution:
+    def reverseList(self, head: ListNode | None) -> ListNode | None:
+        prev = None
+        curr = head
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        return prev
